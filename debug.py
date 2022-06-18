8578f900a254923c23590bf59a830ebe02206466
natsteve124 <107065263+natsteve124@users.noreply.github.com>
2022-06-17 19:17:11 -0700

    update

--- debug.py
+++ debug.py
@@ -14,3 +14,5 @@ print("lone_sum of 10, 10, 10 should be 0: " + str(lone_sum(10, 10, 10)))
 print("lone_sum of 1, 2, 3 should be 6: " + str(lone_sum(1, 2, 3)))
 print("lone_sum of 1, 2, 1 should be 2: " + str(lone_sum(1, 2, 1)))
 print("lone_sum of 4, 5, 6 should be 15: " + str(lone_sum(4, 5, 6)))
+
+# this code run successfully.
